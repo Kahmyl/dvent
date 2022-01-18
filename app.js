84e712d61b822a59de411fca045b9b462523fcdf
Kahmyl <59609015+Kahmyl@users.noreply.github.com>
2022-01-18 12:49:17 +0100

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cors({
     origin:'https://dvents.vercel.app'
 }))
 
-app.use((req, res) => {
+app.use((req, res, next) => {
     res.header(
         "Access-Control-Allow-Headers",
         "x-access-token, Origin, Content-Type, Accept"
